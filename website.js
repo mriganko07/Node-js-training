bb1824e4e78ec7b1569b213a8ea0d912ee64a504
mriganko07 <mrigankaadhikary35@gmail.com>
2026-02-21 22:45:58 +0530

    OK

--- website.js
+++ website.js
@@ -1,23 +1,76 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) =>{
+http.createServer((req, res) => {
 
-    fs.readFile("./html/index.html", 'utf-8', (err, data)=>{
-        if(err){
-            res.writeHead(500, {'content-type': 'text/plain'})
-            res.end("Internal Server Error")
-            return false;
-        }
+    var collectHeaderdata
+
+    // setTimeout(()=>{
+
+    // })
+
+    collectHeaderdata = fs.readFileSync("./html/header.html", 'utf-8', )
+
+    //     (err, headerdata) => {
+
+    //     if (err) {
+    //         res.writeHead(500, { 'content-type': 'text/plain' })
+    //         res.end("Internal Server Error")
+    //         return false;
+    //     }
         
-        res.write(data)
+    //     collectHeaderdata = headerdata
 
+    //     // res.write(headerdata)
 
-        res.end("Chachi")
-        // else if(){
+    //     // res.end()
 
-        // }
-    })
+    // })
+
+    
+    
+    var file = "/index"
+
+    if(req.url != "/"){
+        file = req.url;
+    }
+
+    
+
+    if (req.url != "/style.css") {
+
+        fs.readFile("./html"+file+".html", 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'content-type': 'text/plain' })
+                res.end("Internal Server Error")
+                return false;
+            }
+            
+            // console.log(collectHeaderdata);                
+
+            res.write(collectHeaderdata+data)
+
+            res.end()
+
+        })
+    }
+
+    else if (req.url == "/style.css") {
+
+        fs.readFile("./html/style.css", 'utf-8', (err, data) => {
+
+            if (err) {
+                res.writeHead(500, { 'content-type': 'text/plain' })
+                res.end("CSS not found")
+                return false;
+            }
+
+            res.writeHead(200, { 'content-type': 'text/css' })
+
+            res.end(data)
+
+        })
+    }
 
 
 }).listen(5555)
